56019fd271a48c6cdb0c477afcd31f7add433e64
Joschka Lingemann <joschka.lingemann@cern.ch>
2016-01-12 15:29:26 +0100

    Updating ROOT and gcc version. Adding install target for PCMs.

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 export PATH=/afs/cern.ch/sw/lcg/releases/LCG_72a/fastjet/3.1.1/x86_64-slc6-gcc48-opt/bin:/afs/cern.ch/sw/lcg/contrib/CMake/2.8.9/Linux-i386/bin:${PATH}
-source /afs/cern.ch/sw/lcg/contrib/gcc/4.8.1/x86_64-slc6/setup.sh
-source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.20/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
+source /afs/cern.ch/sw/lcg/contrib/gcc/4.9.3/x86_64-slc6/setup.sh
+source /afs/cern.ch/exp/fcc/sw/0.5/LCG_80/ROOT/6.04.06/x86_64-slc6-gcc49-opt/bin/thisroot.sh
 
 export ANALYSISCPP=$PWD/install
 export LD_LIBRARY_PATH=$ANALYSISCPP/lib:$LD_LIBRARY_PATH
